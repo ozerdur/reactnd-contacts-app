c730b524af09fe253c3bfe750ba1f64c35a14273
TURKCELL\TTODURMAZ <ozer.durmaz@turkcell.com.tr>
2021-12-18 23:46:58 +0300

    prop-types added

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ListContacts = (props) => {
   return (
@@ -27,3 +28,8 @@ const ListContacts = (props) => {
 };
 
 export default ListContacts;
+
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+};
