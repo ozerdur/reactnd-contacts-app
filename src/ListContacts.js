67e2e3d6d3483d0374cf3feea08bb87371618f40
TURKCELL\TTODURMAZ <ozer.durmaz@turkcell.com.tr>
2021-12-18 22:58:14 +0300

    state lesson 1

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
-import reactDom from 'react-dom';
+import React from 'react';
 
-class ListContacts extends Component {
-  render() {
-    return (
-      <ol className="contact-list">
-        {this.props.contacts.map((contact) => (
-          <li key={contact.id} className="contact-list-item">
-            <div
-              className="contact-avatar"
-              style={{
-                backgroundImage: `url(${contact.avatarURL})`,
-              }}
-            />
+const ListContacts = (props) => {
+  return (
+    <ol className="contact-list">
+      {props.contacts.map((contact) => (
+        <li key={contact.id} className="contact-list-item">
+          <div
+            className="contact-avatar"
+            style={{
+              backgroundImage: `url(${contact.avatarURL})`,
+            }}
+          />
 
-            <div className="contact-details">
-              <p>{contact.name}</p>
-              <p>{contact.handle}</p>
-            </div>
-            <button className="contact-remove" />
-          </li>
-        ))}
-      </ol>
-    );
-  }
-}
+          <div className="contact-details">
+            <p>{contact.name}</p>
+            <p>{contact.handle}</p>
+          </div>
+          <button
+            className="contact-remove"
+            onClick={() => props.onDeleteContact(contact)}
+          />
+        </li>
+      ))}
+    </ol>
+  );
+};
 
 export default ListContacts;
